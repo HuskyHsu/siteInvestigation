6efbe2df718cf895df165b260cb66fce3ff3a951
ShihChi Hsu <chessman416@hotmail.com>
2017-07-26 17:03:21 +0800

    V2.2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,59 @@
-let URL = 'https://script.google.com/macros/s/AKfycbwcU5HW37VknYF1BK7BMVO47GQqR6Sw12IHrc9l2WkP95XNSdY/exec'
+let spreadsheetsID = encodeURIComponent(new URLSearchParams(window.location.search).get('spreadsheetsID'));
+let spreadsheetsName = encodeURIComponent(new URLSearchParams(window.location.search).get('spreadsheetsName'));
+let spreadsheets = `spreadsheetsID=${spreadsheetsID}&spreadsheetsName=${spreadsheetsName}`;
+
+// let URL = 'https://script.google.com/macros/s/AKfycbwcU5HW37VknYF1BK7BMVO47GQqR6Sw12IHrc9l2WkP95XNSdY/exec';
+let URL = 'https://script.google.com/macros/s/AKfycbwyLhi5JLkkmFaqCpKUDbysecLBCxzgY-YO8ONSkL0gEVjfcJM/exec';
+
 
 var app = new Vue({
     el: '#app',
     data: {
         fields: [],
         fieldContent: {},
-        ExistingData: getExistingData()
+        ExistingData: getExistingData(),
+        GPSLocation: {},
+        picked: 'GPS'
     },
     methods: {
         add: function() {
+
+            let LatLng = {};
+            if (this.picked == 'GPS'){
+                LatLng = this.GPSLocation;
+            }
+            else {
+                LatLng = map.getCenter();
+            }
+
+            let m = new Date();
+
+            this.fieldContent['緯度'] = LatLng.lat;
+            this.fieldContent['經度'] = LatLng.lng;
+            
+            this.fieldContent['調查時間'] = m.getFullYear() +"/"+ (m.getMonth()+1) +"/"+ m.getDate() + " " + m.getHours() + ":" + m.getMinutes() + ":" + m.getSeconds() + " GMT+0800"
+             
+            let queryStr = `${spreadsheets}&method=PostAdd&` + this.fields.map((value, index) => {
+                if (value.fieldName == '編號') {
+                    return ''
+                }
+	            return encodeURIComponent(value.fieldName) + '=' + encodeURIComponent(this.fieldContent[value.fieldName])
+            }).join('&');
+
+            queryStr = queryStr.replace("&&", "&");
+
+			//Axios
+			axios({
+                method:'post',
+                url: URL,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+			    },
+                data: queryStr
+			}).then(function(response) {
+                getExistingData();
+                alert(response.data);
+			});
 
         },
         edit: function() {
@@ -17,7 +62,7 @@ var app = new Vue({
             
             this.fieldContent['調查時間'] = m.getFullYear() +"/"+ (m.getMonth()+1) +"/"+ m.getDate() + " " + m.getHours() + ":" + m.getMinutes() + ":" + m.getSeconds() + " GMT+0800"
              
-            let queryStr = 'method=edit&' + this.fields.map((value, index) => {
+            let queryStr = `${spreadsheets}&method=PostEdit&` + this.fields.map((value, index) => {
 	            return encodeURIComponent(value.fieldName) + '=' + encodeURIComponent(this.fieldContent[value.fieldName])
             }).join('&');
 
@@ -53,7 +98,10 @@ var app = new Vue({
                 let point = L.marker([i["緯度"], i['經度']])
                     .bindPopup(`編號：${i["編號"]}<br> 經緯度：${i["經度"]}, ${i['緯度']}`)
                     .on('click', onPointClick)
-                    .addTo(ExistingDatalayerGroup);
+                    .addTo(ExistingDatalayerGroup)
+                    .on('click', function clickZoom(e) {
+                        map.setView(e.target.getLatLng(), 16);
+                    });
       
                 if (i["已調查"]) {
                     point._icon.classList.add('sepia');
@@ -75,7 +123,7 @@ var app = new Vue({
 
 axios({
     method: 'get',
-    url: URL + '?method=getFieldNames',
+    url: `${URL}?${spreadsheets}&method=getFieldNames`,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
@@ -102,7 +150,7 @@ axios({
 function getExistingData() {
     axios({
         method: 'get',
-        url: URL + '?method=getExistingData',
+        url: `${URL}?${spreadsheets}&method=getExistingData`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -115,5 +163,3 @@ function getExistingData() {
         app.ExistingData = response.data;
     });
 }
-
-// getExistingData();
--- js/map.js
+++ js/map.js
@@ -2,13 +2,13 @@
 let streets = L.tileLayer('https://mt{s}.google.com/vt/x={x}&y={y}&z={z}&hl=zh-TW', {
     id: 'streets',
     subdomains: "012",
-    attribution: 'Map data: &copy; Google'
+    // attribution: 'Map data: &copy; Google'
 });
 
 let satellite = L.tileLayer('https://mt{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     id: 'topography',
     subdomains: "012",
-    attribution: 'Map data: &copy; Google'
+    // attribution: 'Map data: &copy; Google'
 });
 
 let map = L.map('map', {
@@ -21,6 +21,25 @@ let baseMaps = {
     "Google衛星影像": satellite
 };
 
+let manIcon = [
+    L.icon({
+        iconUrl: 'png/happy-man.png',
+        iconSize: [32, 32],
+        iconAnchor: [16, 30]
+    }),
+    L.icon({
+        iconUrl: 'png/man-exercising-with-arm-raised.png',
+        iconSize: [32, 32], 
+        iconAnchor: [16, 30]
+    }),
+    L.icon({
+        iconUrl: 'png/man-with-two-balloons.png',
+        iconSize: [32, 32],
+        iconAnchor: [16, 30]
+    })
+]
+
+
 
 L.control.layers(baseMaps).addTo(map);
 
@@ -31,10 +50,12 @@ function onLocationFound(e) {
 
     // var loction = L.marker(e.latlng).addTo(map);
     // loction._icon.classList.add('location');
-
-    L.circle(e.latlng, 10).addTo(map);
-
+    // L.circle(e.latlng, 10).addTo(map);
     L.circle(e.latlng, radius).addTo(map);
+    L.marker(e.latlng, {icon: manIcon[parseInt((Math.random() * 3))]}).addTo(map);
+
+    app.GPSLocation = e.latlng
+    map.GPSLocation.textContent = `GPS定位：Lon:${e.latlng.lng.toFixed(6)}, Lat:${e.latlng.lat.toFixed(6)}`;
 }
 
 map.on('locationfound', onLocationFound);
@@ -46,3 +67,51 @@ function onLocationError(e) {
 map.on('locationerror', onLocationError);
 
 let ExistingDatalayerGroup = L.layerGroup([]).addTo(map);
+
+let Location = L.Control.extend({
+
+    options: {position: 'bottomleft'},
+
+    onAdd: function (map) {
+        let div = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+        map.GPSLocation = L.DomUtil.create('p', 'GPSLocation');
+        map.mapCenterLocation = L.DomUtil.create('p', 'mapCenterLocation');
+
+        map.GPSLocation.textContent = `GPS定位：Lon:${map.getCenter().lng.toFixed(6)}, Lat:${map.getCenter().lat.toFixed(6)}`;
+        map.mapCenterLocation.textContent = `地圖中心：Lon:${map.getCenter().lng.toFixed(6)}, Lat:${map.getCenter().lat.toFixed(6)}`;
+
+        div.appendChild(map.GPSLocation);
+        div.appendChild(map.mapCenterLocation);
+
+        return div;
+    }
+});
+map.addControl(new Location());
+
+let Update = L.Control.extend({
+
+    options: {position: 'topright'},
+
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+
+        container.style.backgroundColor = 'white';
+        container.style.backgroundImage = "url(png/refresh-button.png)";
+        container.style.backgroundSize = "44px 44px";
+        container.style.width = '44px';
+        container.style.height = '44px';
+
+        container.onclick = function () {getExistingData();}
+
+        return container;
+    }
+});
+map.addControl(new Update());
+
+map.on("moveend", function () {
+    map.mapCenterLocation.textContent = `地圖中心：Lon:${map.getCenter().lng.toFixed(6)}, Lat:${map.getCenter().lat.toFixed(6)}`;
+});
+
+let centerMarker = L.DomUtil.create('div', 'centerMarker');
+centerMarker.textContent = "＋"
+document.getElementById('map').appendChild(centerMarker);
