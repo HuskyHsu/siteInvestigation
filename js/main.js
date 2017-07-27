d958d7dfcab3dbe6e8a890656a477f8a6b051a46
ShihChi Hsu <chessman416@hotmail.com>
2017-07-27 13:05:24 +0800

    add google導航

--- js/main.js
+++ js/main.js
@@ -145,11 +145,15 @@ var app = new Vue({
 
             this.ExistingData.forEach(i => {
 
+                let googleNavigation = `https://www.google.com.tw/maps/dir/${i["緯度"]},${i['經度']}/${this.GPSLocation.lat},${this.GPSLocation.lng}/@24,120.5,10z/data=!3m1!4b1!4m2!4m1!3e0`;
+
+                
                 let point = L.marker([i["緯度"], i['經度']])
-                    .bindPopup(`編號：${i["編號"]}`)
-                    .on('click', onPointClick)
+                    .bindPopup(`編號：${i["編號"]}<br><a href=${googleNavigation} target="_blank">google導航</a>`)
+                    // .on('click', onPointClick)
                     .addTo(ExistingDatalayerGroup)
                     .on('click', function clickZoom(e) {
+                        app.fieldContent = i
                         map.setView(e.target.getLatLng(), 16);
                     });
       
@@ -157,9 +161,9 @@ var app = new Vue({
                     point._icon.classList.add('sepia');
                 }
 
-                function onPointClick() {
-                    app.fieldContent = i
-                }
+                // function onPointClick() {
+                //     app.fieldContent = i
+                // }
                 // ExistingDatalayerGroup.addLayer(LG);
 
                 avgLonLat[0] += i["緯度"];
